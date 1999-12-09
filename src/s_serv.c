1532bbe0493b6814fa58d29edb6cfc5b7c0c0ac9
Lucas Madar <lucas@negaverse.org>
1999-12-09 21:25:47 +0000

    Erm, fixed where m_trace would coredump. Nice, seddy.
    git-svn-id: svn+ssh://localhost/home/epiphani/home/dalrepo/bahamut/trunk@179 57140981-5af7-11db-9af8-080020d9fdd4

--- src/s_serv.c
+++ src/s_serv.c
@@ -4079,13 +4079,15 @@ m_trace(aClient *cptr, aClient *sptr, int parc, char *parv[])
    tname = (parc > 1) ? parv[1] : me.name;
 
 #ifdef HIDEULINEDSERVS
-   acptr = next_client_double(client, tname);
-   if (!(IsAnOper(sptr)) && IsULine(acptr))
+   if((acptr = next_client_double(client, tname)))
    {
-	sendto_one(sptr, err_str(ERR_NOPRIVILEGES), me.name, parv[0]);
-	return 0;
+      if (!(IsAnOper(sptr)) && IsULine(acptr))
+      {
+         sendto_one(sptr, err_str(ERR_NOPRIVILEGES), me.name, parv[0]);
+         return 0;
+      }
+      acptr = NULL; /* shrug, we borrowed it, reset it just in case */
    }
-   acptr = NULL; /* shrug, we borrowed it, reset it just in case */
 #endif
 
    if (parc > 2)
