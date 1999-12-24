43b0bc477c9fec441c850f462bc838c14d08fd38
Aaron Wiebe <epiphani@gmail.com>
1999-12-24 00:26:38 +0000

    Devel stuff... called this 1.9-BETA.. considering the amount of crap we've got planned, i assume this will become 2.0
    git-svn-id: svn+ssh://localhost/home/epiphani/home/dalrepo/bahamut/trunk@196 57140981-5af7-11db-9af8-080020d9fdd4

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -27,13 +27,13 @@
 #define STABLE  3
 #define BETA    4
 
-#define BRANCHSTATUS RELEASE
+#define BRANCHSTATUS BETA
 
 #define BASENAME "bahamut"
 #define BRANCH "pelennor"
 #define MAJOR 1
-#define MINOR 2
-#define PATCH 2
+#define MINOR 9
+#define PATCH 0
 
 #define PATCH1 \
 \
