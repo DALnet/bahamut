a74c8c2db440cfa25f6a1bd5618e2dd696f23697
Lucas Madar <lucas@negaverse.org>
2000-04-12 15:36:53 +0000

    *grumble* I just love to flood your inbox!
    git-svn-id: svn+ssh://localhost/home/epiphani/home/dalrepo/bahamut/trunk@334 57140981-5af7-11db-9af8-080020d9fdd4

--- src/list.c
+++ src/list.c
@@ -216,7 +216,7 @@ aClient    *make_client(aClient *from, aClient *uplink)
       cptr->status = STAT_UNKNOWN;
       cptr->fd = -1;
       cptr->uplink = uplink;
-      (void) strcpy(cptr->username, "unknown");  only in local structs now
+      (void) strcpy(cptr->username, "unknown");
       cptr->since = cptr->lasttime = cptr->firsttime = timeofday;
       /*
        * cptr->confs = NULL; 
