40db18b25fbd9f382bae6306f0116e95d91ff918
Lucas Madar <lucas@negaverse.org>
2000-12-30 01:14:27 +0000

    Get our CAPABs straight here
    git-svn-id: svn+ssh://localhost/home/epiphani/home/dalrepo/bahamut/trunk@384 57140981-5af7-11db-9af8-080020d9fdd4

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -810,7 +810,7 @@ static int completed_connection(aClient * cptr)
 
    /* pass on our capabilities to the server we /connect'd */
 
-   sendto_one(cptr, "CAPAB TS3 NOQUIT SSJOIN BURST UNCONNECT NEGO");
+   sendto_one(cptr, "CAPAB TS3 NOQUIT SSJOIN BURST UNCONNECT DKEY");
 
    aconf = find_conf(cptr->confs, cptr->name, CONF_NOCONNECT_SERVER);
    if (!aconf) {
