0ef8812fdefbd4d89163019882a4001fdec1d276
Lucas Madar <lucas@negaverse.org>
2001-01-13 04:59:57 +0000

    amazing how one character can make such a huge difference..
    git-svn-id: svn+ssh://localhost/home/epiphani/home/dalrepo/bahamut/trunk@444 57140981-5af7-11db-9af8-080020d9fdd4

--- src/s_user.c
+++ src/s_user.c
@@ -2968,7 +2968,7 @@ m_whois(aClient *cptr,
 					  parv[0], name,
 					  user->username, user->host, acptr->info);
 
-                if(user->real_oper_host && (IsAdmin(sptr) || (sptr == cptr)) )
+                if(user->real_oper_host && (IsAdmin(sptr) || (sptr == acptr)) )
                 {
                    sendto_one(sptr, rpl_str(RPL_WHOISACTUALLY),
                               me.name, sptr->name, name, 
