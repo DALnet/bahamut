29c7dd3d6973e00c7ec8410458e2ee322e85bebd
Aaron Wiebe <epiphani@gmail.com>
2003-03-09 19:27:22 +0000

    i am a meat popsicle.
    ac_* functions
    
    my mind is mush.  i am a cut-and-paste wizard.  the amount of time
    it took to write this commit log is disturbingly long.
    
    
    git-svn-id: svn+ssh://localhost/home/epiphani/home/dalrepo/bahamut/trunk@852 57140981-5af7-11db-9af8-080020d9fdd4

---
+++ include/structfunc.h
@@ -0,0 +1,96 @@
+/* include/structfunc.h
+ * Copyright(c) 2003, Aaron Wiebe
+ * Bahamut Development Team
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id$ */
+
+/* This file includes external function defines for aClient (and possibly
+ * other) structure references. */
+
+extern aClient *ac_next(aClient *);
+extern aClient *ac_prev(aClient *);
+extern anUser *ac_user(aClient *);
+extern aServer *ac_server(aClient *);
+extern aWhoWas *ac_whowas(aClient *);
+extern aClient *ac_from(aClient *);
+extern aClient *ac_uplink(aClient *);
+extern time_t ac_lasttime(aClient *);
+extern time_t ac_firsttime(aClient *);
+extern time_t ac_since(aClient *);
+extern ts_val ac_tsinfo(aClient *);
+extern int ac_fd(aClient *);
+extern int ac_hopcount(aClient *);
+extern short ac_status(aClient *);
+extern char *ac_nicksent(aClient *);
+extern char *ac_name(aClient *);
+extern char *ac_info(aClient *);
+#ifdef FLUD
+extern Link *ac_fludees(aClient *);
+#endif
+extern struct in_addr ac_ip(aClient *);
+extern char *ac_hostip(aClient *);
+extern Link *ac_watch(aClient *);
+extern int ac_watches(aClient *);
+/******************************************
+ * These are the local functions...
+ ******************************************/
+extern int ac_count(aClient *);
+#ifdef FLUD
+extern time_t ac_fludblock(aClient *);
+extern struct fludbot *ac_fluders(aClient *);
+#endif
+#ifdef ANTI_SPAMBOT
+extern time_t ac_last_join_time(aClient *);
+extern time_t ac_last_leave_time(aClient *);
+extern int ac_join_leave_count(aClient *);
+extern int ac_oper_warn_count_down(aClient *);
+#endif
+extern char *ac_buffer(aClient *);
+extern short ac_lastsq(aClient *);
+extern struct DBuf *ac_sendQ(aClient *);
+extern struct DBuf *ac_recvQ(aClient *);
+extern long ac_sendM(aClient *);
+extern long ac_sendK(aClient *);
+extern long ac_recieveM(aClient *);
+extern long ac_recieveK(aClient *);
+extern u_short ac_sendB(aClient *);
+extern u_short ac_recieveB(aClient *);
+extern long ac_lastrecvM(aClient *);
+extern int ac_priority(aClient *);
+extern aClient *ac_acpt(aClient *);
+extern Link *ac_confs(aClient *);
+extern int ac_authfd(aClient *);
+extern char *ac_username(aClient *);
+extern unsigned short ac_port(aClient *);
+extern struct hostent *ac_hostp(aClient *);
+#ifdef ANTI_NICK_FLOOD
+extern time_t ac_last_nick_change(aClient *);
+extern int ac_number_of_nick_changes(aClient *);
+#endif
+#ifdef NO_AWAY_FLUD
+extern time_t ac_alas(aClient *);
+extern int ac_acount(aClient *);
+#endif
+extern char *ac_sockhost(aClient *);
+extern char *ac_passwd(aClient *);
+extern int ac_oflag(aClient *);
+extern int ac_sockerr(aClient *);
+extern int ac_capabilities(aClient *);
+extern int ac_pingval(aClient *);
+extern int ac_sendqlen(aClient *);
+
---
+++ src/struct.c
@@ -0,0 +1,402 @@
+/* src/struct.c
+ * Copyright(c) 2003, Aaron Wiebe
+ * Bahamut development team
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id$ */
+
+/* This file contains quick and dirty functions for retriving information
+ * from structures. */
+
+
+#include struct.h
+
+
+aClient *ac_next(aClient *cptr)
+{
+	return cptr->next;
+}
+
+aClient *ac_prev(aClient *cptr)
+{
+	return cptr->prev;
+}
+
+anUser *ac_user(aClient *cptr)
+{
+	return ac->user;
+}
+
+aServer *ac_server(aClient *cptr)
+{
+	return cptr->serv;
+}
+
+aWhoWas *ac_whowas(aClient *cptr)
+{
+	return cptr->whowas;
+}
+
+aClient *ac_from(aClient *cptr)
+{
+	return cptr->from;
+}
+
+aClient *ac_uplink(aClient *cptr)
+{
+	return cptr->uplink;
+}
+
+time_t ac_lasttime(aClient *cptr)
+{
+	return cptr->lasttime;
+}
+
+time_t ac_firsttime(aClient *cptr)
+{
+	return cptr->firsttime;
+}
+
+time_t ac_since(aClient *cptr)
+{
+	return cptr->since;
+}
+
+ts_val ac_tsinfo(aClient *cptr)
+{
+	return cptr->tsinfo;
+}
+
+int ac_fd(aClient *cptr)
+{
+	return cptr->fd;
+}
+
+int ac_hopcount(aClient *cptr)
+{
+	return cptr->hopcount;
+}
+
+short ac_status(aClient *cptr)
+{
+	return cptr->status;
+}
+
+char *ac_nicksent(aClient *cptr)
+{
+	return cptr->nicksent;
+}
+
+char *ac_name(aClient *cptr)
+{
+	return cptr->name;
+}
+
+char *ac_info(aClient *cptr)
+{
+	return cptr->info;
+}
+
+#ifdef FLUD
+Link *ac_fludees(aClient *cptr)
+{
+	return cptr->fludees;
+}
+#endif
+
+struct in_addr ac_ip(aClient *cptr)
+{
+	return cptr->ip;
+}
+
+char *ac_hostip(aClient *cptr)
+{
+	return cptr->hostip;
+}
+
+Link *ac_watch(aClient *cptr)
+{
+	return cptr->watch;
+}
+
+int ac_watches(aClient *cptr)
+{
+	return cptr->watches;
+}
+
+/*************************************
+ * local only stuff starts here 
+ *************************************/
+
+int ac_count(aClient *cptr)
+{
+	if(cptr->fd == -2)
+		abort();
+	return cptr->count;
+}
+
+#ifdef FLUD
+time_t ac_fludblock(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->fludblock;
+}
+
+struct fludbot *ac_fluders(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->fluders;
+}
+#endif
+#ifdef ANTI_SPAMBOT
+time_t ac_last_join_time(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->last_join_time;
+}
+
+time_t ac_last_leave_time(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->last_leave_time;
+}
+
+int ac_join_leave_count(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->join_leave_count;
+}
+
+int ac_oper_warn_count_down(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->oper_warn_count_down;
+}
+#endif
+
+char *ac_buffer(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->buffer;
+}
+
+short ac_lastsq(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->lastsq;
+}
+
+struct DBuf *ac_sendQ(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->sendQ;
+}
+
+struct DBuf *ac_recvQ(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->recvQ;
+}
+
+long ac_sendM(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->sendM;
+}
+
+long ac_sendK(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->sendK;
+}
+
+long ac_recieveM(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->recieveM;
+}
+
+long ac_recieveK(aClient *cptr)
+{
+	if(cptr->fd == -2)
+		abort();
+	return cptr->recieveK;
+}
+
+u_short ac_sendB(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->sendB;
+}
+
+u_short ac_recieveB(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->recieveB;
+}
+
+long ac_lastrecvM(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->lastrecvM;
+}
+
+int ac_priority(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->priority;
+}
+
+aClient *ac_acpt(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->acpt;
+}
+
+Link *ac_confs(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->confs;
+}
+
+int ac_authfd(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->authfd;
+}
+
+char *ac_username(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->username;
+}
+
+unsigned short ac_port(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->port;
+}
+
+struct hostent *ac_hostp(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->hostp;
+}
+
+#ifdef ANTI_NICK_FLOOD
+time_t ac_last_nick_change(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->last_nick_change;
+}
+
+int ac_number_of_nick_changes(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->number_of_nick_changes;
+}
+#endif
+#ifdef NO_AWAY_FLUD
+time_t ac_alas(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->alas;
+}
+
+int ac_acount(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->acount;
+}
+
+#endif
+
+char *ac_sockhost(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->sockhost;
+}
+
+char *ac_passwd(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->passwd;
+}
+
+int ac_oflag(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->oflag;
+}
+
+int ac_sockerr(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->sockerr;
+}
+
+int ac_capabilities(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->capabilities;
+}
+
+int ac_pingval(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->pingval;
+}
+
+int ac_sendqlen(aClient *cptr)
+{
+        if(cptr->fd == -2)
+                abort();
+        return cptr->sendqlen;
+}
