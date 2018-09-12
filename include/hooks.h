c2ef3652ab3bb560536a60a3ada8546730a932e6
Kobi Shmueli <kobi@dal.net>
2018-09-12 09:15:20 +0300

    Updated MODULE_INTERFACE_VERSION.

--- include/hooks.h
+++ include/hooks.h
@@ -84,7 +84,7 @@ enum c_hooktype {
 extern int call_hooks(enum c_hooktype hooktype, ...);
 extern int init_modules();
 
-#define MODULE_INTERFACE_VERSION 1008 /* the interface version (hooks, modules.c commands, etc) */
+#define MODULE_INTERFACE_VERSION 1009 /* the interface version (hooks, modules.c commands, etc) */
 
 #ifdef BIRCMODULE
 extern void *bircmodule_add_hook(enum c_hooktype, void *, void *);
