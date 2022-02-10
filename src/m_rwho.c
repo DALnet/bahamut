d44755abce08ed6a70bd35335b85382313c1554c
Emilio A. Escobar <eescobar@gmail.com>
2022-02-09 19:11:20 -0800

    Moving ! inside parenthesis

--- src/m_rwho.c
+++ src/m_rwho.c
@@ -995,7 +995,7 @@ static int rwho_match(aClient *cptr, int *failcode, aClient **failclient)
         return 0;
 
     if ((rwho_opts.check[1] & RWM_HOST) &&
-        !(rwho_opts.host_func[1](rwho_opts.host_pat[1], cptr->user->host) || !rwho_opts.host_func[1](rwho_opts.host_pat[1], cptr->user->mhost)))
+        (!rwho_opts.host_func[1](rwho_opts.host_pat[1], cptr->user->host) || !rwho_opts.host_func[1](rwho_opts.host_pat[1], cptr->user->mhost)))
         return 0;
 #else
     if ((rwho_opts.check[0] & RWM_HOST) &&
