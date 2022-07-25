b8930705c176ddce7ac5157146ed0e24f2e8b87d
Emilio A. Escobar <eescobar@gmail.com>
2022-07-25 21:56:58 +0200

    show TLS encrypted in stats ?

--- src/m_stats.c
+++ src/m_stats.c
@@ -314,6 +314,9 @@ serv_info(aClient *cptr, char *name)
         if(RC4EncLink(acptr))
             sendto_one(cptr, ":%s %d %s : - RC4 encrypted", me.name, 
                         RPL_STATSDEBUG, name);
+        else(IsSSL(acptr))
+            sendto_one(cptr, ":%s %d %s : - TLS encrypted", me.name,
+                        RPL_STATSDEBUG, name);
 
         if(ZipOut(acptr))
         {
