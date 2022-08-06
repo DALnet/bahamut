ed6587c0936323bdad8a13d3420d69836530ec1f
Emilio A. Escobar <eescobar@gmail.com>
2022-08-06 13:54:03 -0700

    Getting subject name as an object for debugging

--- src/ssl.c
+++ src/ssl.c
@@ -469,8 +469,9 @@ int ssl_verify_callback(int preverify_ok, X509_STORE_CTX *ctx)
 		/*
 		 * for testing, must delete
 		 */
-		for (int i = 0; i < X509_NAME_entry_count(buf); i++) {
-			X509_NAME_ENTRY *e = X509_NAME_get_entry(buf, i);
+		X509_NAME *subj = X509_get_subject_name(cert);
+		for (int i = 0; i < X509_NAME_entry_count(subj); i++) {
+			X509_NAME_ENTRY *e = X509_NAME_get_entry(subj, i);
 			ASN1_STRING *d = X509_NAME_ENTRY_get_data(e);
 			char *str = ASN1_STRING_data(d);
 			sendto_realops_lev(DEBUG_LEV, "SSL: Entry %d - %s", i, str);
