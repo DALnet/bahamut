c63ae2c4273ca23fdff34583119a5c86f7752098
Emilio Escobar <eescobar@gmail.com>
2025-03-15 20:13:06 -0700

    Changed header of files

--- src/m_sasl.c
+++ src/m_sasl.c
@@ -1,5 +1,5 @@
 /************************************************************************
- *   IRC - Internet Relay Chat, src/s_ircv3.c
+ *   IRC - Internet Relay Chat, src/m_sasl.c
  *   Copyright (C) 1990 Jarkko Oikarinen and
  *                      University of Oulu, Computing Center
  *
