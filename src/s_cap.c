488a271ac694b14efcb6711491d0ae09c11f52ba
Emilio Escobar <eescobar@gmail.com>
2025-03-15 20:13:06 -0700

    Changed header of files

--- src/s_cap.c
+++ src/s_cap.c
@@ -1,5 +1,5 @@
 /************************************************************************
- *   IRC - Internet Relay Chat, src/s_ircv3.c
+ *   IRC - Internet Relay Chat, src/s_cap.c
  *   Copyright (C) 1990 Jarkko Oikarinen and
  *                      University of Oulu, Computing Center
  *
