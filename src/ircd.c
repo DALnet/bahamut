2b15fc1142042b9e815f0b25fa647dd8852be107
Emilio A. Escobar <eescobar@gmail.com>
2025-09-23 14:00:54 -0700

    adding code definition to make res functions work

--- src/ircd.c
+++ src/ircd.c
@@ -18,6 +18,8 @@
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#define _POSIX_C_SOURCE 200809L
+#define _DEFAULT_SOURCE
 #include "struct.h"
 #include "common.h"
 #include "sys.h"
